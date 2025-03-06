2e9599ea81c030f7993f2fa68e5ae283514934c0
Tonasj <87764307+Tonasj@users.noreply.github.com>
2025-03-06 13:32:06 +0200

    Updated program.cs with azure test

--- AzureTestPipeline/Program.cs
+++ AzureTestPipeline/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World! Azure");
+app.MapGet("/", () => "Hello World! Azure test");
 
 
 
